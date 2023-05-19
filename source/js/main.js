9f0197c94f6373083a9a47b7ed0e69e112c92a60
muskratysh <muskratysh@gmail.com>
2023-05-19 15:14:56 +0300

    попытки исправить свайпер

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,7 @@ import {Form} from './modules/form-validate/form';
 import initVideo from './utils/init-video';
 import {initTabs} from './utils/init-tabs';
 import {initTrainerSwiper} from './utils/init-slider';
+import './vendor/swiper';
 
 // ---------------------------------
 
